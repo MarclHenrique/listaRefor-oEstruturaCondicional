c244a6aaec8f3d38fcf4b55065590cea0c49f183
MarclHenrique <mh593417@gmail.com>
2024-08-03 10:46:02 -0300

    Ano bissexto

--- anobissexto.js
+++ anobissexto.js
@@ -2,16 +2,25 @@ let prompt = require("prompt-sync")();
 
 let ano = Number(prompt("Digite um ano: "));
 
-if (ano % 4 === 0) { //2024 
-    if (ano % 100 === 0) { // 24 True
-        if (ano % 400 === 0) { //True 
-            console.log("Ano bissexto"); //ok
-        } else {
-            console.log("Ano não bissexto");
-        }
-    } else {
-        console.log("Ano bissexto");
-    }
-} else {
-    console.log("Ano não bissexto"); 
+// if (ano % 4 === 0) { //2024 
+//     if (ano % 100 === 0) { // 24 True
+//         if (ano % 400 === 0) { //True 
+//             console.log("Ano bissexto"); //ok
+//         } else {
+//             console.log("Ano não bissexto");
+//         }
+//     } else {
+//         console.log("Ano bissexto");
+//     }
+// } else {
+//     console.log("Ano não bissexto"); 
+// }
+
+//Exercio Resolvido na aula
+
+if ((ano % 4 === 0 && ano % 100 !== 0 ) || (ano % 400 === 0)){
+    console.log(`${ano} é bissexto`);
+}else{
+    console.log(`${ano} não é bissexto`);
+    
 }
